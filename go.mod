91c9d6c6209880ae71161437bfae13f6eb3a8e83
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-02-08 20:38:26 -0300

    finding raw json properties from OAS file and unmarshalling to model structure

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/gustavomassa/oas-diff
 go 1.17
 
 require github.com/santhosh-tekuri/jsonschema/v5 v5.0.0
+
+require (
+	github.com/tidwall/gjson v1.14.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,11 +7,42 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/gustavomassa/oas-diff/model"
 	"github.com/santhosh-tekuri/jsonschema/v5"
 	_ "github.com/santhosh-tekuri/jsonschema/v5/httploader"
+	"github.com/tidwall/gjson"
 )
 
-const OAS_SCHEMA_URL = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/schemas/v3.1/schema.json"
+const (
+	OAS_SCHEMA_URL   = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/schemas/v3.1/schema.json"
+	OAS_INFO_KEY     = "info"
+	OAS_SERVERS_KEY  = "servers"
+	OAS_PATHS_KEY    = "paths"
+	OAS_WEBHOOKS_KEY = "webhooks"
+)
+
+func isSliceOfUniqueItems(xs []interface{}) bool {
+	s := len(xs)
+	m := make(map[string]struct{}, s)
+	for _, x := range xs {
+		key, _ := json.Marshal(&x)
+		m[string(key)] = struct{}{}
+	}
+	return s == len(m)
+}
+
+func getJsonPathData(jsonData []byte, path string) (result []byte, err error) {
+	node := gjson.GetBytes(jsonData, path)
+	if !node.Exists() {
+		return result, fmt.Errorf("failed to find json path: %s", path)
+	}
+	if node.Index > 0 {
+		result = jsonData[node.Index : node.Index+len(node.Raw)]
+	} else {
+		result = []byte(node.Raw)
+	}
+	return result, nil
+}
 
 func main() {
 	compiler := jsonschema.NewCompiler()
@@ -22,13 +53,13 @@ func main() {
 		log.Fatalf("%#v", err)
 	}
 
-	data, err := ioutil.ReadFile("test/shipping_invalid.json")
+	jsonData, err := ioutil.ReadFile("test/shipping_valid.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var v interface{}
-	if err := json.Unmarshal(data, &v); err != nil {
+	if err := json.Unmarshal(jsonData, &v); err != nil {
 		log.Fatal(err)
 	}
 
@@ -49,4 +80,33 @@ func main() {
 			log.Fatalf("%#v", err)
 		}
 	}
+
+	infoSch := sch.Properties[OAS_INFO_KEY]
+	serversSch := sch.Properties[OAS_SERVERS_KEY]
+	pathsSch := sch.Properties[OAS_PATHS_KEY]
+	webhooksSch := sch.Properties[OAS_WEBHOOKS_KEY]
+
+	fmt.Println(infoSch.String())
+	fmt.Println(serversSch.String())
+	fmt.Println(pathsSch.String())
+	fmt.Println(webhooksSch.String())
+
+	if !gjson.ValidBytes(jsonData) {
+		panic("invalid json")
+	}
+
+	infoData, err := getJsonPathData(jsonData, OAS_INFO_KEY)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(infoData))
+
+	var infoModel model.Info
+	err = json.Unmarshal(infoData, &infoModel)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(infoModel)
 }

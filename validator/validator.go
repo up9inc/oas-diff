5d63999b20706adbee9774b8f238d6b00fd04270
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-02-14 12:22:16 -0300

    re-factoring

---
+++ validator/validator.go
@@ -0,0 +1,55 @@
+package validator
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/santhosh-tekuri/jsonschema/v5"
+	file "github.com/up9inc/oas-diff/json"
+)
+
+type Validator interface {
+	InitOAS31Schema() error
+	Validate(jsonFile file.JsonFile) error
+}
+
+type validator struct {
+	compiler   *jsonschema.Compiler
+	schema     *jsonschema.Schema
+	jsonSchema file.JsonFile
+}
+
+func NewValidator() Validator {
+	v := &validator{
+		compiler:   jsonschema.NewCompiler(),
+		schema:     nil,
+		jsonSchema: nil,
+	}
+	v.compiler.Draft = jsonschema.Draft2020
+
+	return v
+}
+
+func (v *validator) Validate(jsonFile file.JsonFile) error {
+	if v.schema == nil {
+		err := v.InitOAS31Schema()
+		if err != nil {
+			return err
+		}
+	}
+
+	if jsonFile == nil {
+		return errors.New("json file is nil")
+	}
+
+	if jsonFile.GetData() == nil {
+		return errors.New("json file data is nil")
+	}
+
+	var d interface{}
+	if err := json.Unmarshal(*jsonFile.GetData(), &d); err != nil {
+		return err
+	}
+
+	return v.schema.Validate(d)
+}

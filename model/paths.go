455091447c6cff89ae1cb8fb413c37085baf1aa3
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-02-21 12:50:27 -0300

    paths diff tags

--- model/paths.go
+++ model/paths.go
@@ -12,74 +12,73 @@ var _ Model = (*Paths)(nil)
 
 type Paths map[string]*PathItem
 
-// TODO: Add diff targs
 // TODO: Add identifiers to arrays
 type PathItem struct {
-	Ref         string     `json:"$ref,omitempty"`
-	Summary     string     `json:"summary,omitempty"`
-	Description string     `json:"description,omitempty"`
-	Connect     *Operation `json:"connect,omitempty"`
-	Delete      *Operation `json:"delete,omitempty"`
-	Get         *Operation `json:"get,omitempty"`
-	Head        *Operation `json:"head,omitempty"`
-	Options     *Operation `json:"options,omitempty"`
-	Patch       *Operation `json:"patch,omitempty"`
-	Post        *Operation `json:"post,omitempty"`
-	Put         *Operation `json:"put,omitempty"`
-	Trace       *Operation `json:"trace,omitempty"`
-	Servers     Servers    `json:"servers,omitempty"`
-	Parameters  Parameters `json:"parameters,omitempty"`
+	Ref         string     `json:"$ref,omitempty" diff:"$ref"`
+	Summary     string     `json:"summary,omitempty" diff:"summary"`
+	Description string     `json:"description,omitempty" diff:"description"`
+	Connect     *Operation `json:"connect,omitempty" diff:"connect"`
+	Delete      *Operation `json:"delete,omitempty" diff:"delete"`
+	Get         *Operation `json:"get,omitempty" diff:"get"`
+	Head        *Operation `json:"head,omitempty" diff:"head"`
+	Options     *Operation `json:"options,omitempty" diff:"options"`
+	Patch       *Operation `json:"patch,omitempty" diff:"patch"`
+	Post        *Operation `json:"post,omitempty" diff:"post"`
+	Put         *Operation `json:"put,omitempty" diff:"put"`
+	Trace       *Operation `json:"trace,omitempty" diff:"trace"`
+	Servers     Servers    `json:"servers,omitempty" diff:"servers"`
+	Parameters  Parameters `json:"parameters,omitempty" diff:"parameters"`
 }
 
 type Operation struct {
-	Summary      string                `json:"summary,omitempty"`
-	Description  string                `json:"description,omitempty"`
-	ExternalDocs *ExternalDocs         `json:"externalDocs,omitempty"`
-	Tags         []string              `json:"tags,omitempty"`
-	OperationID  string                `json:"operationId,omitempty"`
-	Parameters   Parameters            `json:"parameters,omitempty"`
-	Responses    map[string]*Response  `json:"responses"`
-	Consumes     []string              `json:"consumes,omitempty"`
-	Produces     []string              `json:"produces,omitempty"`
-	Security     *SecurityRequirements `json:"security,omitempty"`
+	Summary      string                `json:"summary,omitempty" diff:"summary"`
+	Description  string                `json:"description,omitempty" diff:"description"`
+	ExternalDocs *ExternalDocs         `json:"externalDocs,omitempty" diff:"externalDocs"`
+	Tags         []string              `json:"tags,omitempty" diff:"tags"`
+	OperationID  string                `json:"operationId,omitempty" diff:"operationId"`
+	Parameters   Parameters            `json:"parameters,omitempty" diff:"parameters"`
+	Responses    map[string]*Response  `json:"responses" diff:"responses"`
+	Consumes     []string              `json:"consumes,omitempty" diff:"consumes"`
+	Produces     []string              `json:"produces,omitempty" diff:"produces"`
+	Security     *SecurityRequirements `json:"security,omitempty" diff:"security"`
 }
 
 type Parameters []*Parameter
 
 type Parameter struct {
-	Ref              string        `json:"$ref,omitempty"`
-	In               string        `json:"in,omitempty"`
-	Name             string        `json:"name,omitempty"`
-	Description      string        `json:"description,omitempty"`
-	CollectionFormat string        `json:"collectionFormat,omitempty"`
-	Type             string        `json:"type,omitempty"`
-	Format           string        `json:"format,omitempty"`
-	Pattern          string        `json:"pattern,omitempty"`
-	AllowEmptyValue  bool          `json:"allowEmptyValue,omitempty"`
-	Required         bool          `json:"required,omitempty"`
-	UniqueItems      bool          `json:"uniqueItems,omitempty"`
-	ExclusiveMin     bool          `json:"exclusiveMinimum,omitempty"`
-	ExclusiveMax     bool          `json:"exclusiveMaximum,omitempty"`
-	Schema           *SchemaRef    `json:"schema,omitempty"`
-	Items            *SchemaRef    `json:"items,omitempty"`
-	Enum             []interface{} `json:"enum,omitempty"`
-	MultipleOf       *float64      `json:"multipleOf,omitempty"`
-	Minimum          *float64      `json:"minimum,omitempty"`
-	Maximum          *float64      `json:"maximum,omitempty"`
-	MaxLength        *uint64       `json:"maxLength,omitempty"`
-	MaxItems         *uint64       `json:"maxItems,omitempty"`
-	MinLength        uint64        `json:"minLength,omitempty"`
-	MinItems         uint64        `json:"minItems,omitempty"`
-	Default          interface{}   `json:"default,omitempty"`
+	Ref              string        `json:"$ref,omitempty" diff:"$ref"`
+	In               string        `json:"in,omitempty" diff:"in"`
+	Name             string        `json:"name,omitempty" diff:"name"`
+	Description      string        `json:"description,omitempty" diff:"description"`
+	CollectionFormat string        `json:"collectionFormat,omitempty" diff:"collectionFormat"`
+	Type             string        `json:"type,omitempty" diff:"type"`
+	Format           string        `json:"format,omitempty" diff:"format"`
+	Pattern          string        `json:"pattern,omitempty" diff:"pattern"`
+	AllowEmptyValue  bool          `json:"allowEmptyValue,omitempty" diff:"allowEmptyValue"`
+	Required         bool          `json:"required,omitempty" diff:"required"`
+	UniqueItems      bool          `json:"uniqueItems,omitempty" diff:"uniqueItems"`
+	ExclusiveMin     bool          `json:"exclusiveMinimum,omitempty" diff:"exclusiveMinimum"`
+	ExclusiveMax     bool          `json:"exclusiveMaximum,omitempty" diff:"exclusiveMaximum"`
+	Schema           *SchemaRef    `json:"schema,omitempty" diff:"schema"`
+	Items            *SchemaRef    `json:"items,omitempty" diff:"items"`
+	Enum             []interface{} `json:"enum,omitempty" diff:"enum"`
+	MultipleOf       *float64      `json:"multipleOf,omitempty" diff:"multipleOf"`
+	Minimum          *float64      `json:"minimum,omitempty" diff:"minimum"`
+	Maximum          *float64      `json:"maximum,omitempty" diff:"maximum"`
+	MaxLength        *uint64       `json:"maxLength,omitempty" diff:"maxLength"`
+	MaxItems         *uint64       `json:"maxItems,omitempty" diff:"maxItems"`
+	MinLength        uint64        `json:"minLength,omitempty" diff:"minLength"`
+	MinItems         uint64        `json:"minItems,omitempty" diff:"minItems"`
+	Default          interface{}   `json:"default,omitempty" diff:"default"`
 }
 
 type Response struct {
-	Ref         string             `json:"$ref,omitempty"`
-	Description string             `json:"description,omitempty"`
-	Schema      *SchemaRef         `json:"schema,omitempty"`
-	Headers     map[string]*Header `json:"headers,omitempty"`
+	Ref         string             `json:"$ref,omitempty" diff:"$ref"`
+	Description string             `json:"description,omitempty" diff:"description"`
+	Schema      *SchemaRef         `json:"schema,omitempty" diff:"schema"`
+	Headers     map[string]*Header `json:"headers,omitempty" diff:"headers"`
 	// TODO: 3.1 examples is an object
-	Examples map[string]interface{} `json:"examples,omitempty"`
+	Examples map[string]interface{} `json:"examples,omitempty" diff:"examples"`
 }
 
 type Header struct {

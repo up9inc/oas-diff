32a16e1a3d9ccc73391748180f21ab78ff06e9e1
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-03-02 21:32:11 -0300

    parameters differ

---
+++ differentiator/parametersDiffer.go
@@ -0,0 +1,34 @@
+package differentiator
+
+import (
+	"reflect"
+
+	lib "github.com/r3labs/diff/v2"
+	"github.com/up9inc/oas-diff/model"
+)
+
+type ParametersDiffer struct {
+	opts   DifferentiatorOptions
+	differ *lib.Differ
+
+	DiffFunc (func(path []string, a, b reflect.Value, p interface{}) error)
+}
+
+func NewParameterDiffer(opts DifferentiatorOptions) *ParametersDiffer {
+	return &ParametersDiffer{
+		opts:   opts,
+		differ: nil,
+	}
+}
+
+func (p *ParametersDiffer) Match(a, b reflect.Value) bool {
+	return lib.AreType(a, b, reflect.TypeOf(model.Parameters{}))
+}
+
+func (p *ParametersDiffer) Diff(cl *lib.Changelog, path []string, a, b reflect.Value) error {
+	return p.differ.DiffSlice(path, a, b)
+}
+
+func (p *ParametersDiffer) InsertParentDiffer(dfunc func(path []string, a, b reflect.Value, p interface{}) error) {
+	p.DiffFunc = dfunc
+}

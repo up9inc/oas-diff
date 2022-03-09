2d25d4fba743120a3c39c17c5697d01eaf7d7e53
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-03-09 00:36:16 -0300

    basic acceptance test for the loose logic

--- acceptanceTests/diff_test.go
+++ acceptanceTests/diff_test.go
@@ -16,6 +16,8 @@ const (
 	OAS_SCHEMA_FILE = "../validator/oas31.json"
 	FILE1           = "data/simple.json"
 	FILE2           = "data/simple2.json"
+	FILE_LOOSE1     = "data/simple_loose.json"
+	FILE_LOOSE2     = "data/simple_loose2.json"
 )
 
 type DiffSuite struct {
@@ -38,16 +40,6 @@ func (d *DiffSuite) SetupTest() {
 		return
 	}
 
-	d.jsonFile1 = file.NewJsonFile(FILE1)
-	d.jsonFile2 = file.NewJsonFile(FILE2)
-
-	d.vall = validator.NewValidator()
-	err = d.vall.InitOAS31Schema(OAS_SCHEMA_FILE)
-	if err != nil {
-		d.T().Error(err)
-		return
-	}
-
 	// must be set on each test because of the options
 	d.diff = nil
 }
@@ -56,11 +48,30 @@ func TestDiffSuite(t *testing.T) {
 	suite.Run(t, new(DiffSuite))
 }
 
-func SimpleDiff(d *DiffSuite, opts differentiator.DifferentiatorOptions) {
+func validateDependencies(d *DiffSuite) {
+	if d.jsonFile1 == nil {
+		d.T().Error("jsonFile1 is nil, you must initialize jsonFile1 for each test")
+		return
+	}
+
+	if d.jsonFile2 == nil {
+		d.T().Error("jsonFile2 is nil, you must initialize jsonFile2 for each test")
+		return
+	}
+
+	if d.vall == nil {
+		d.T().Error("vall is nil, you must initialize vall for each test")
+		return
+	}
+
 	if d.diff == nil {
 		d.T().Error("diff is nil, you must initialize diff for each test")
 		return
 	}
+}
+
+func SimpleDiff(d *DiffSuite, opts differentiator.DifferentiatorOptions) {
+	validateDependencies(d)
 
 	assert := d.Assert()
 
@@ -250,7 +261,101 @@ func SimpleDiff(d *DiffSuite, opts differentiator.DifferentiatorOptions) {
 	assert.Equal("custom uuid", paths[index].To)
 }
 
+func SimpleDiffLoose(d *DiffSuite, opts differentiator.DifferentiatorOptions) {
+	validateDependencies(d)
+
+	assert := d.Assert()
+
+	assert.True(opts.Loose, "--loose flag should be true")
+
+	_, err := d.jsonFile1.Read()
+	assert.NoError(err)
+
+	_, err = d.jsonFile2.Read()
+	assert.NoError(err)
+
+	output, err := d.diff.Diff(d.jsonFile1, d.jsonFile2)
+	assert.NoError(err, fmt.Sprintf("diff error: %v", err))
+	assert.NotNil(output, "changeMap is nil")
+	assert.Len(output, 3, "changeMap len should be 3")
+	assert.NotNil(output[model.OAS_INFO_KEY], fmt.Sprintf("failed to find changeMap key '%s'", model.OAS_INFO_KEY))
+	assert.NotNil(output[model.OAS_SERVERS_KEY], fmt.Sprintf("failed to find changeMap key '%s'", model.OAS_SERVERS_KEY))
+	assert.NotNil(output[model.OAS_PATHS_KEY], fmt.Sprintf("failed to find changeMap key '%s'", model.OAS_PATHS_KEY))
+
+	// aux vars
+	index := -1
+	identifier := ""
+	property := ""
+
+	// info
+	info := output[model.OAS_INFO_KEY]
+	assert.Len(info, 1, "info should have 1 change")
+
+	// info[0]
+	index = 0
+	assert.Equal("update", info[index].Type)
+	assert.Len(info[index].Path, 1)
+	assert.Equal([]string{"version"}, info[index].Path)
+	assert.Equal("1.0.0", info[index].From)
+	assert.Equal("1.1.0", info[index].To)
+
+	// servers
+	servers := output[model.OAS_SERVERS_KEY]
+	assert.Len(servers, 1, "servers should have 1 change")
+
+	// servers[0] -> position 0
+	index = 0
+	identifier = "some url"
+	property = "description"
+	assert.Equal("update", servers[index].Type)
+	if opts.IncludeFilePath {
+		assert.Len(servers[index].Path, 4)
+		assert.Equal([]string{fmt.Sprintf("%s/%s", d.absPath, FILE_LOOSE1), model.OAS_SERVERS_KEY, identifier, property}, servers[index].Path)
+	} else {
+		assert.Len(servers[index].Path, 2)
+		assert.Equal([]string{identifier, property}, servers[index].Path)
+	}
+	assert.Equal(differentiator.Identifier{
+		model.Servers{}.GetIdentifierName(): identifier,
+	}, servers[index].Identifier)
+	assert.Equal("SOME DESC", servers[index].From)
+	assert.Equal("some desc updated", servers[index].To)
+
+	// paths
+	paths := output[model.OAS_PATHS_KEY]
+	assert.Len(paths, 1, "paths should have 1 change")
+
+	// paths[0]
+	index = 0
+	identifier = "accept"
+	basePath := []string{"/users", "get", "parameters", identifier}
+	property = "required"
+	assert.Equal("update", paths[index].Type)
+	if opts.IncludeFilePath {
+		assert.Len(paths[index].Path, 7)
+		assert.Equal([]string{fmt.Sprintf("%s/%s", d.absPath, FILE1), model.OAS_PATHS_KEY, basePath[0], basePath[1], basePath[2], basePath[3], property}, paths[index].Path)
+	} else {
+		assert.Len(paths[index].Path, 5)
+		assert.Equal([]string{basePath[0], basePath[1], basePath[2], basePath[3], property}, paths[index].Path)
+	}
+	assert.Equal(differentiator.Identifier{
+		model.Parameters{}.GetIdentifierName(): identifier,
+	}, paths[index].Identifier)
+	assert.Equal(false, paths[index].From)
+	assert.Equal(true, paths[index].To)
+}
+
 func (d *DiffSuite) TestSimpleDiffWithFullFilePath() {
+	d.jsonFile1 = file.NewJsonFile(FILE1)
+	d.jsonFile2 = file.NewJsonFile(FILE2)
+
+	d.vall = validator.NewValidator()
+	err := d.vall.InitOAS31Schema(OAS_SCHEMA_FILE)
+	if err != nil {
+		d.T().Error(err)
+		return
+	}
+
 	opts := differentiator.DifferentiatorOptions{
 		IncludeFilePath: true,
 	}
@@ -259,9 +364,38 @@ func (d *DiffSuite) TestSimpleDiffWithFullFilePath() {
 }
 
 func (d *DiffSuite) TestSimpleDiffWithoutFilePath() {
+	d.jsonFile1 = file.NewJsonFile(FILE1)
+	d.jsonFile2 = file.NewJsonFile(FILE2)
+
+	d.vall = validator.NewValidator()
+	err := d.vall.InitOAS31Schema(OAS_SCHEMA_FILE)
+	if err != nil {
+		d.T().Error(err)
+		return
+	}
+
 	opts := differentiator.DifferentiatorOptions{
 		IncludeFilePath: false,
 	}
 	d.diff = differentiator.NewDifferentiator(d.vall, opts)
 	SimpleDiff(d, opts)
+}
+
+func (d *DiffSuite) TestSimpleLooseDiffWithoutFilePath() {
+	d.jsonFile1 = file.NewJsonFile(FILE_LOOSE1)
+	d.jsonFile2 = file.NewJsonFile(FILE_LOOSE2)
+
+	d.vall = validator.NewValidator()
+	err := d.vall.InitOAS31Schema(OAS_SCHEMA_FILE)
+	if err != nil {
+		d.T().Error(err)
+		return
+	}
+
+	opts := differentiator.DifferentiatorOptions{
+		IncludeFilePath: false,
+		Loose:           true,
+	}
+	d.diff = differentiator.NewDifferentiator(d.vall, opts)
+	SimpleDiffLoose(d, opts)
 }

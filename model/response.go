0a0fb3b8e5f395a9ffeecb2b0590a4275a168ade
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-03-09 19:01:43 -0300

    updated response model, added missing content struct

--- model/response.go
+++ model/response.go
@@ -1,10 +1,8 @@
 package model
 
+// TODO: Add Links?
 type Response struct {
-	Ref         string                 `json:"$ref,omitempty" diff:"$ref"`
-	Description string                 `json:"description,omitempty" diff:"description"`
-	Schema      *Schema                `json:"schema,omitempty" diff:"schema"`
-	Headers     Headers                `json:"headers,omitempty" diff:"headers"`
-	Example     interface{}            `json:"example,omitempty" diff:"example"`
-	Examples    map[string]interface{} `json:"examples,omitempty" diff:"examples"`
+	Description string  `json:"description,omitempty" diff:"description"`
+	Headers     Headers `json:"headers,omitempty" diff:"headers"`
+	Content     Content `json:"content,omitempty" diff:"content"`
 }

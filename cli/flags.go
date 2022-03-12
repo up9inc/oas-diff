5981a95a00e06614460c10509ac231ba6bd8ca9a
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-03-12 00:23:19 -0300

    changed the name of the input files to base-file and second-file

--- cli/flags.go
+++ cli/flags.go
@@ -4,12 +4,12 @@ import "github.com/urfave/cli/v2"
 
 var (
 	FileFlag = &cli.StringFlag{
-		Name:     "file",
-		Usage:    "Path of the OAS 3.1 file",
+		Name:     "base-file",
+		Usage:    "Path of the base OAS 3.1 file",
 		Required: true,
 	}
 	FileFlag2 = &cli.StringFlag{
-		Name:     "file2",
+		Name:     "second-file",
 		Usage:    "Path of the second OAS 3.1 file",
 		Required: true,
 	}

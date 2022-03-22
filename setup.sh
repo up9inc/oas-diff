588bd2fe5c43b4202d737715f8cf6dd0d7754cc9
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-03-22 20:05:15 -0300

    added make build to setup

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 
 echo "Cloning diff project to lib folder..."
 mkdir lib || true && git clone --single-branch --branch feature/public_diff_functions https://github.com/up9inc/diff.git lib
+
+echo "Building oas-diff.."
+make build

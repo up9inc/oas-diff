9c812388b41a0dccb2cd9ae7adbf4916daa92d41
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-03-22 20:41:35 -0300

    updated loose flag description

--- cli/flags.go
+++ cli/flags.go
@@ -5,35 +5,35 @@ import "github.com/urfave/cli/v2"
 var (
 	BaseFileFlag = &cli.StringFlag{
 		Name:     "base-file",
-		Usage:    "Path of the base OAS 3.1 file",
+		Usage:    "path of the base OAS 3.1 file",
 		Required: true,
 	}
 	SecondFileFlag = &cli.StringFlag{
 		Name:     "second-file",
-		Usage:    "Path of the second OAS 3.1 file",
+		Usage:    "path of the second OAS 3.1 file",
 		Required: true,
 	}
 	LooseFlag = &cli.BoolFlag{
 		Name:     "loose",
-		Usage:    "loosely diff",
+		Usage:    "loosely diff, ignores global case sensitivity for strings comparisons and ignore headers that start with 'x-' and 'user-agent'",
 		Required: false,
 		Value:    false,
 	}
 	IncludeFilePathFlag = &cli.BoolFlag{
 		Name:     "include-file-path",
-		Usage:    "Whether or not to include the full file path from the diff changelog",
+		Usage:    "whether or not to include the full file path from the diff changelog",
 		Required: false,
 		Value:    false,
 	}
 	ExcludeDescriptionsFlag = &cli.BoolFlag{
 		Name:     "exclude-descriptions",
-		Usage:    "Whether or not to exclude descriptions from the diff changelog",
+		Usage:    "whether or not to exclude descriptions from the diff changelog",
 		Required: false,
 		Value:    false,
 	}
 	HtmlOutputFlag = &cli.BoolFlag{
 		Name:     "html",
-		Usage:    "save the changelog file as a html report",
+		Usage:    "save an html report",
 		Required: false,
 	}
 )
